13c3304278f457433be5b75b38603604b6e46976
Yannis Rohloff <yannis@uni-bremen.de>
2016-12-31 21:32:15 +0100

    saver

--- 23.py
+++ 23.py
@@ -1,20 +1,28 @@
+from saver import *
 
-
-
-def divisors(n):
+def abundant(n):
     divsum = 1
-    for i in range(2,n):
-        if n%i==0:
+    for i in range(2,int(n/2)):
+        if n%i == 0:
             divsum += i
-        if i < divsum:
-            return False
-    return i < divsum
+        if n < divsum:
+            return True
+    return False
 
-for i in range(28123)
-    if abundant(i):
-        pass
 
-print(abundant(()))
+save(2)
+exit()
+abundants = {}
+for i in range(28123):
+    abundants[i] = abundant(i)
+
+
+
+limit = 28123
+for i in range(limit, 0, -1):
+    #print(i)
+    pass
+print("done")
 
 
 
