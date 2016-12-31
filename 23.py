6d19cc5e5d4cf26bad959e18f4b4e9373c85d692
Yannis Rohloff <yannis@uni-bremen.de>
2016-12-31 23:14:52 +0100

    added saver and have too improve 21 and 23

--- 23.py
+++ 23.py
@@ -1,28 +1,61 @@
 from saver import *
+import itertools
+
+s = Saver()
+#s.ignore()
+
+limit = 28123
+# calculated
+limit = 20162
 
 def abundant(n):
     divsum = 1
-    for i in range(2,int(n/2)):
+    for i in range(2,int(n)):
         if n%i == 0:
             divsum += i
         if n < divsum:
             return True
     return False
 
+abundants = s.load("abun")
+if abundants == None:
+    abundants = list()
+    for i in range(limit+1):
+        if abundant(i):
+            abundants.append(i)
+    s.save("abun", abundants)
 
-save(2)
-exit()
-abundants = {}
-for i in range(28123):
-    abundants[i] = abundant(i)
+r_abundants = abundants[::-1]
+
+def find_pair(a1, s):
+    for a2 in r_abundants:
+        if a1+a2 < s:
+            return False
+        if a1+a2 == s:
+            print(a1,"+", a2, "=", s)
+            return True
+
+    return False
 
 
+# Solution hardly inspired by http://blog.dreamshire.com/project-euler-23-solution/
+# Precalculation the abundants was stupid
+# Also i didn't realize that i can use the implication as it was used there
+# a in abn
+# forall b: (b-a) not in abn
+# => b is summable
+abn = set()
+s = 0
+for i in range(20161+1):
+    print(i)
+    if abundant(i):
+        abn.add(i)
+    if not any( (i-a in abn) for a in abn):
+        s += i
 
-limit = 28123
-for i in range(limit, 0, -1):
-    #print(i)
-    pass
-print("done")
+print(s)
+
+
 
 
 
