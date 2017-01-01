cc42f376d3c47607c607dc7372f509364f092feb
Yannis Rohloff <yannis@uni-bremen.de>
2017-01-01 22:17:47 +0100

    increased speed

--- 5.py
+++ 5.py
@@ -3,16 +3,20 @@ import math
 import numpy
 from EulerVerify import verify
 
-def findSmallest_wrong():
-    for i in count(1):
-        k = [i%x for x in range(1,20)]
-        print(k)
-        if max([i%x for x in range(1,20)])==0:
-            return i
-
+# ca 1.6 - 2.0s
 def findSmallest():
     for i in range(1,20):
         for p in combinations(range(1,20), i):
+            n = numpy.prod(p)
+            if max([n%x for x in range(1,20)])==0:
+                print(p)
+                return n
+
+
+def findSmallest():
+    relevant = [2,3,4,5,11,12,13,14,15,16,17,18,19,20]
+    for i in range(3,20):
+        for p in combinations(relevant, i):
             n = numpy.prod(p)
             if max([n%x for x in range(1,20)])==0:
                 print(p)
