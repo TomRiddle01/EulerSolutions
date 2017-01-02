5716406d87983e9deafcb9e5ef10f4ca61be2343
Yannis Rohloff <yannis@uni-bremen.de>
2017-01-02 02:30:12 +0100

    bla

--- 50.py
+++ 50.py
@@ -1,59 +1,25 @@
 from itertools import *
 import numpy
+from Euler import * 
 
-def isPrime(n):
-    if n in primes:
-        return True
-    if n < 0:
-        return False
-    if n == 1:
-        return True
-    for i in range(2,n-1):
-        if n%i==0:
-            return False
-    return True
+isprime = prime_sieve(1000000)
+primes = [i for i,p in enumerate(isprime) if p]
 
-def nextPrime(n):
-    for i in count(1):
-        if isPrime(n+i):
-            return n+i
 
-prime = []
-prime.append(False)
-prime.append(False)
-primes = []
-
-i = 1
-s = 0
-while s < 1000000:
-    if not i%1000:
-        print(i)
-    if isPrime(i):
-        prime.append(True)
-        primes.append(i)
-        s += i
-    else:
-        prime.append(False)
-    i += 1
-
-primes_set = set(primes)
-
-print("calculatted primes")
-m = 0
+m = 21
 r = 0
 for a in range(len(primes)-21):
-    for b in range(a+21,len(primes)):
-        arr = primes[a:b]
+    if a*m > 1000000:
+        break
+    for b in range(m,len(primes)):
+        arr = primes[a:a+b]
         num = sum(arr)
 
-        if num > 1000000:
+        if num >= 1000000:
             break
 
-        if isPrime(num):
+        if isprime[num]:
             if len(arr) > m:
                 m = len(arr)
                 r = num
-                print(m,r)
-
-
-print(m,r)
+verify(r)
