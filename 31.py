003955d38faab25710f73dfb78647ae2c8f2d128
Yannis Rohloff <yannis@uni-bremen.de>
2017-01-05 19:08:05 +0100

    31

---
+++ 31.py
@@ -0,0 +1,18 @@
+from Euler import *
+import itertools
+
+
+price = 200
+ways = 0
+
+for a in range(price, -1, -200):
+    for b in range(a, -1, -100):
+        for c in range(b, -1, -50):
+            for d in range(c, -1, -20):
+                for e in range(d, -1, -10):
+                    for f in range(e, -1, -5):
+                        for g in range(f, -11, -2):
+                            ways+=1
+
+verify(ways)
+
