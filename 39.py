762116ab4c6b9293747e64c10c622e79f6cd2c14
Yannis Rohloff <yannis@uni-bremen.de>
2017-01-07 17:39:14 +0100

    only fast in pypy

---
+++ 39.py
@@ -0,0 +1,19 @@
+from Euler import *
+m = 120
+
+rm = 0
+rr = 0
+for m in range(1,1001):
+    sols = 0
+    for a in range(1, m):
+        for b in range(1, m-a):
+            c = (m-a-b)
+            if a**2 + b**2 == c**2:
+                sols += 1
+
+    if rr < sols:
+        rm = m
+        rr = sols
+verify(rm)
+
+
