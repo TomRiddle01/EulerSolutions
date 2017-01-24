a7451a82638ab82493b54aad7c67ef5f631bb0ed
Yannis Rohloff <yannis@uni-bremen.de>
2017-01-24 19:08:45 +0100

    made flake8 valid

--- EulerVerify.py
+++ EulerVerify.py
@@ -1,4 +1,4 @@
-import glob   
+import glob
 import re
 import subprocess
 import hashlib
@@ -11,7 +11,9 @@ import inspect
 
 starttime = time.time()
 
+
 class bcolors:
+
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -21,12 +23,18 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def ok(str):
     print(bcolors.OKGREEN + str + bcolors.ENDC)
+
+
 def warning(str):
     print(bcolors.WARNING + str + bcolors.ENDC)
+
+
 def fail(str):
     print(bcolors.FAIL + str + bcolors.ENDC)
+
 
 class EulerVerify:
     def __init__(self):
@@ -34,14 +42,17 @@ class EulerVerify:
         self.scan()
 
     def scan(self):
-        files=glob.glob("./solutions/*.py")
-        match = lambda s: re.match("./solutions/([0-9]*).py", s)
-        self.files = dict((int(match(file).group(1)), file) for file in files if match(file))
+        files = glob.glob("./solutions/*.py")
+
+        def match(s): re.match("./solutions/([0-9]*).py", s)
+
+        self.files = dict((int(match(file).group(1)), file)
+                          for file in files if match(file))
 
         with open("hashes.txt") as f:
             self.hashes = [line.strip() for line in f.readlines()]
 
-    def executeAll(self, timeout = 10, commit_id = None):
+    def executeAll(self, timeout=10, commit_id=None):
         failed = False
         for i in range(max(self.files.keys())):
             if i in self.files:
@@ -50,7 +61,7 @@ class EulerVerify:
         if failed:
             exit(1)
 
-    def execute(self, num, timeout = 10, commit_id = None):
+    def execute(self, num, timeout=10, commit_id=None):
         if num in self.files:
             if commit_id and not self.file_changed(num, commit_id):
                 return
@@ -58,12 +69,13 @@ class EulerVerify:
             print("Running Euler Problem #%d" % (num))
             self.solution = ""
 
-            p = subprocess.Popen(["python3", "solutions/"+str(num)+".py"], \
-                stdout=subprocess.PIPE, \
-                stderr=subprocess.PIPE)
+            p = subprocess.Popen(["python3", "solutions/"+str(num)+".py"],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
             t = time.time()
             self.out = ""
             self.err = ""
+
             def target():
                 out, err = p.communicate("")
                 lines = str(out, "UTF-8").split("\n")
@@ -90,7 +102,8 @@ class EulerVerify:
                         warning("\tPassed in %.3f" % t)
                     return True
                 else:
-                    fail("\tWrong: Solution %s: %s != %s" % (self.solution, self.hashes[num-1], hash))
+                    fail("\tWrong: Solution %s: %s != %s"
+                         % (self.solution, self.hashes[num-1], hash))
                     print(self.out)
                     print(self.err)
                     return False
@@ -98,7 +111,9 @@ class EulerVerify:
     def _verify(self, value):
         t = time.time()-starttime
         file = inspect.stack()[2][1]
-        match = lambda s: re.match("solutions/([0-9]*).py", s)
+
+        def match(s): re.match("solutions/([0-9]*).py", s)
+
         num = int(match(file).group(1))
         hash = hashlib.md5(str(value).encode("utf-8")).hexdigest()
         if self.hashes[num-1] == hash:
@@ -113,29 +128,35 @@ class EulerVerify:
         exit()
 
     def file_changed(self, num, commit_id):
-        p = subprocess.Popen(["git", "diff", commit_id, "--", str(num)+".py"], \
-            stdout=subprocess.PIPE, \
-            stderr=subprocess.PIPE)
-        t = time.time()
+        p = subprocess.Popen(["git", "diff",
+                             commit_id, "--",
+                             str(num)+".py"],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
         out, err = p.communicate("")
 
         return len(out) != 0
+
 
 def verify(value):
     EulerVerify()._verify(value)
 
 
-
-
 if len(sys.argv) > 1:
     timeout = 10
     parser = argparse.ArgumentParser(description='Process some integers.')
+
     parser.add_argument('option', type=str, metavar="option", nargs=1,
                         help='number or all')
-    parser.add_argument('-t', dest='timeout', default=[10], type=int, metavar="seconds",  nargs=1,
+
+    parser.add_argument('-t', dest='timeout', default=[10],
+                        type=int, metavar="seconds",  nargs=1,
                         help='timeout for problem execution')
-    parser.add_argument('-c', dest='commit_id', default=[None], type=str, metavar="commit",  nargs=1,
-                        help='only execute scripts changed since this commit it')
+
+    parser.add_argument('-c', dest='commit_id', default=[None],
+                        type=str, metavar="commit",  nargs=1,
+                        help='only execute scripts changed'
+                        + 'since this commit it')
 
     args = parser.parse_args()
     if "all" in args.option:
--- solutions/Euler.py
+++ solutions/Euler.py
@@ -1,13 +1,15 @@
 import sys
 sys.path.append(".")
-import EulerVerify
+import EulerVerify # noqa
 
 
 def verify(value):
     EulerVerify.EulerVerify()._verify(value)
 
+
 primes = set()
 learned_primes = 0
+
 
 # Lucas Lehmer Test
 def is_prime(n):
@@ -18,11 +20,12 @@ def is_prime(n):
     if n == 1:
         primes.add(n)
         return True
-    for i in range(2,n-1):
-        if n%i==0:
+    for i in range(2, n-1):
+        if n % i == 0:
             return False
     primes.add(n)
     return True
+
 
 def prime_sieve(below):
     isprime = [True]*below
@@ -34,11 +37,13 @@ def prime_sieve(below):
                 isprime[n] = False
     return isprime
 
+
 def init_primes(below):
     global learned_primes, primes
     isprime = prime_sieve(below)
     for p, prime in enumerate(isprime):
-        if prime: primes.add(p)
+        if prime:
+            primes.add(p)
     learned_primes = below
 
 
@@ -50,13 +55,15 @@ def find_divisor(num):
     for p in primes:
         if p <= 1:
             continue
-        if num%p==0:
+        if num % p == 0:
             return p
     raise Exception("primes array not initialized")
 
+
 def prime_factors(num):
     factors = []
-    if num == 0: raise ValueError()
+    if num == 0:
+        raise ValueError()
 
     while not is_prime(num):
         p = find_divisor(num)
@@ -64,6 +71,7 @@ def prime_factors(num):
         num = int(num/p)
     factors.append(num)
     return factors
+
 
 def div_count(n):
     for i in range(learned_primes, n+1):
@@ -76,21 +84,14 @@ def div_count(n):
         else:
             a[p] = 2
     r = 1
-    for k,i in a.items():
+    for k, i in a.items():
         r *= i
     return r
 
 
 def div_sum(n):
-    divsum = 0 
-    for i in range(1,n):
-        if n%i==0:
-            divsum += i
-    return divsum
-
-def div_sum(n):
-    divsum = 0 
-    for i in range(1,n):
-        if n%i==0:
+    divsum = 0
+    for i in range(1, n):
+        if n % i == 0:
             divsum += i
     return divsum
